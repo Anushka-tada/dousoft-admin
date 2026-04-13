bf96d7461418729b9839496b4f01145a3777c8b7
Anushka-tada <tadaanushka@gmail.com>
2026-04-13 20:18:18 +0530

    fixed

--- app/api/home/public/route.js
+++ app/api/home/public/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/db";
-import Home from "@/models/Home";
+import { connectDB } from "@/lib/mongodb";
+import HomeSchema from "@/models/Homepage";
 
 export async function GET() {
   try {
